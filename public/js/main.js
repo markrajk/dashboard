c8f00fb6bc320dbcac14dc4a4bd7d91c4257d0f5
markrajk <marko88rajkovic@gmail.com>
2020-03-25 21:38:01 +0100

    close on small menu

--- public/js/main.js
+++ public/js/main.js
@@ -10,3 +10,15 @@ sidenavMenuToggle.addEventListener('click', function() {
     sidenavMenu.classList.toggle('sidebar-menu-close-anim');
     sidenavMenuToggleIcon.classList.toggle('sidebar-menu-close-icon-rotate');
 })
+
+function closeMenuOnSmall() {
+    if(window.innerWidth < 991) {
+        sidenavMenu.classList.add('sidebar-menu-close-anim');
+        sidenavMenuToggleIcon.classList.add('sidebar-menu-close-icon-rotate');
+        console.log('<991')
+    }
+}
+
+window.addEventListener('resize', closeMenuOnSmall);
+
+closeMenuOnSmall();
